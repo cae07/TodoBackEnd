5d8d4fe1b997c0d07cbfd3c7116c829202e6d60a
Cae <eduardokae01@hotmail.com>
2022-02-12 15:43:51 -0300

    adiciona middleware de erro e ativa o json body parser no index

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const handleError = require('./Middlewares/handleError');
 
 require('dotenv').config();
 
 const app = express();
 const PORT = PROCESS.ENV.port || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+app.use(handleError);
