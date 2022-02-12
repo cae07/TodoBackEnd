31760df0bc070d5c8d8f2151ae814aae738e3fb3
Cae <eduardokae01@hotmail.com>
2022-02-12 19:11:23 -0300

    refatora middleware auth para status e mensagem

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,11 +1,14 @@
 const { verifyToken } = require('../Service/auth.service');
+const { UNAUTHORIZED } = require('../Dictionary/status');
+const { TOKEN_FAIL } = require('../Dictionary/errorMessages');
 
 module.exports = (req, res, next) => {
   try {
-    const failedToken = res.status(401).json({ message: 'jwt malformed'});
+    const failedToken = res.status(UNAUTHORIZED).json({ message: TOKEN_FAIL });
+
     const { authorization } = req.headers;
 
-    if (authorization) return failedToken;
+    if (!authorization) return failedToken;
 
     const user = verifyToken(authorization);
     if (!user) return failedToken;
