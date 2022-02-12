bfa3c906ebcb9852ccfb292270754fce5c586284
Cae <eduardokae01@hotmail.com>
2022-02-12 19:04:26 -0300

    cria arquivo com mensagens de erro no componente dictionary

---
+++ Dictionary/errorMessages.js
@@ -0,0 +1,13 @@
+const ERROR_FIELD = 'Invalid entries.';
+const FIELDS_MISS = 'All fields must be filled.';
+const ERROR_LOGIN = 'Incorrect username or password.';
+const TOKEN_FAIL = 'jwt mal formed.';
+const TOKEN_MISS = 'Token não encontrado.';
+
+module.exports = {
+  ERROR_FIELD,
+  FIELDS_MISS,
+  ERROR_LOGIN,
+  TOKEN_FAIL,
+  TOKEN_MISS,
+};
