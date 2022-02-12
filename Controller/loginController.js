269cea2bddcd1f5beb6642278f7b3a77737ef92f
Cae <eduardokae01@hotmail.com>
2022-02-12 19:25:51 -0300

    adiciona verifyExistUser no loginControler

--- Controller/loginController.js
+++ Controller/loginController.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const auth = require('../Middlewares/auth');
+const { verifyUser, verifyExistUser } = require('../Service/user.service');
+const { OK } = require('../Dictionary/status');
 const { tokenGenerator } = require('../Service/auth.service');
+
 
 const router = express.Router();
 
@@ -8,8 +11,9 @@ router.post('/', auth, async (req, res, next) => {
   try {
     const { email, password } = req.body;
     await verifyUser(email, password);
+    const user = await verifyExistUser(email, password);
     
-    res.status(200).json({ token: 'ok!' });
+    res.status(OK).json({ token: 'ok!' });
   } catch (error) {
     next(error);
   }
