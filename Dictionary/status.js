b11fe2ec627bf36d3f78d6c72bc03896a565324b
Cae <eduardokae01@hotmail.com>
2022-02-12 18:57:21 -0300

    cria o componente dictionary com status

---
+++ Dictionary/status.js
@@ -0,0 +1,22 @@
+// respostas de sucesso
+const OK = 200;
+const CREATED = 201;
+
+// respostas de erro
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const NOT_FOUND = 404;
+const UNPROCESSABLE_ENTITY = 422;
+
+// erro de servidor
+const INTERNAL_SERVER_ERROR = 500
+
+module.exports = {
+  OK,
+  CREATED,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  NOT_FOUND,
+  UNPROCESSABLE_ENTITY,
+  INTERNAL_SERVER_ERROR,
+};
