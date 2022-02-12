6a0e6908dbe39e526e746f679c3028f1eb06571b
Cae <eduardokae01@hotmail.com>
2022-02-12 18:49:28 -0300

    inicia a função verifyUser no componente service / user.service

--- Service/user.service.js
+++ Service/user.service.js
@@ -1,5 +1,17 @@
-const verifyUser = (email, password) => {
+const Joi = require('joi');
 
+const userSchemma = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).alphanum(),
+});
+
+const verifyUser = (email, password) => {
+  const { error } = userSchemma.validate({ email, password });
+
+  if (error) {
+    throw (error);
+  }
+  return true;
 };
 
 module.exports = {
