d9d59e10ef4051cc2931acb2f674890c91841892
Cae <eduardokae01@hotmail.com>
2022-02-13 21:15:44 -0300

    tests: add test para rota de post tasks

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -52,12 +52,13 @@ describe('GET /tasks', () => {
     });
   });
 
-  describe('6- Caso token não informado', () => {
+  describe('7- Caso token não informado', () => {
     let response = {};
     before(async () => {
       response = await chai.request(server)
       .get('/tasks');
     });
+
     it('Retorna status 401', () => {
       expect(response).to.have.property('status');
       expect(response).to.have.status(401);
@@ -66,6 +67,152 @@ describe('GET /tasks', () => {
     it('Retorna a mensagem "jwt mal formed."', () => {
       expect(response.body).to.have.property('message');
       expect(response.body.message).to.equal('jwt mal formed.');
+    });
+  });
+});
+
+describe('POST /tasks', () => {
+  before(async () => {
+    const connection = await getConnection();
+    sinon.stub(MongoClient, 'connect')
+      .resolves(connection);
+  });
+  
+  after(async () => {
+    await MongoClient.connect.restore();
+  });
+  describe('8- Caso de sucesso', () => {
+    before(async () => {
+      await chai.request(server)
+      .post('/createUser')
+      .send({
+        email: 'test@gmail.com',
+        password: '123456',
+      });
+
+      const { body } = await chai.request(server)
+      .post('/login')
+      .send({
+        email: 'test@gmail.com',
+        password: '123456',
+      });
+
+      response = await chai.request(server)
+      .post('/tasks')
+      .set({ "Authorization": `${body.token}` })
+      .send({ task: 'Comprar coquinho' });
+    });
+
+    it('Retorna um objeto', () => {
+      expect(response).to.be.a('object');
+    });
+
+    it('Retorna status 201', () => {
+      expect(response).to.have.property('status');
+      expect(response).to.have.status(201);
+    });
+
+    it('Retorna a nova tarefa', () => {
+      expect(response.body).to.have.property('id');
+      expect(response.body).to.have.property('task');
+      expect(response.body).to.have.property('status');
+      expect(response.body.status).to.equal('pendente');
+    });
+  });
+
+  describe('9- Caso de falha', () => {
+    describe('Quando não existir o campo "task"', () => {
+      let response = {};
+      let token = '';
+
+      before(async () => {
+        await chai.request(server)
+        .post('/createUser')
+        .send({
+          email: 'test@gmail.com',
+          password: '123456',
+        });
+  
+        const { body } = await chai.request(server)
+        .post('/login')
+        .send({
+          email: 'test@gmail.com',
+          password: '123456',
+        });
+
+        token = body.token;
+      });
+
+      it('Retorna status 400', async () => {
+        response = await chai.request(server)
+        .post('/tasks')
+        .set({ "Authorization": token })
+        .send({ "": 'Comprar coquinho' });
+
+        expect(response.error).to.have.property('status');
+        expect(response.error).to.have.status(400);
+      });
+
+      it('Retorna mensagem "task is required"', () => {
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.equal('"task" is required');
+      });
+    });
+
+    describe('Quando o campo "task" estiver vazia', () => {
+      let response = {};
+      let token = '';
+
+      before(async () => {
+        await chai.request(server)
+        .post('/createUser')
+        .send({
+          email: 'test@gmail.com',
+          password: '123456',
+        });
+  
+        const { body } = await chai.request(server)
+        .post('/login')
+        .send({
+          email: 'test@gmail.com',
+          password: '123456',
+        });
+
+        token = body.token;
+      });
+
+      it('Retorna status 400', async () => {
+        response = await chai.request(server)
+        .post('/tasks')
+        .set({ "Authorization": token })
+        .send({ "task": '' });
+
+        expect(response.error).to.have.property('status');
+        expect(response.error).to.have.status(400);
+      })
+
+      it('Retorna mensagem /"task" is not allowed to be empty/', () => {
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.equal('"task" is not allowed to be empty');
+      });
+    });
+
+    describe('Caso token não informado', () => {
+      let response = {};
+      before(async () => {
+        response = await chai.request(server)
+        .get('/tasks');
+      });
+
+      it('Retorna status 401', () => {
+        expect(response).to.have.property('status');
+        expect(response).to.have.status(401);
+      });
+  
+      it('Retorna a mensagem "jwt mal formed."', () => {
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.equal('jwt mal formed.');
+      });
     });
   });
 });
