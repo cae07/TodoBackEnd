da4d003d1ebc72a67021f0ad2c8855c0ef15946d
Cae <eduardokae01@hotmail.com>
2022-02-19 10:35:12 -0300

    refactor: cria pasta de integraçao na de test, transfere os arquivos e cria pasta unity

--- tests/Integration/login.test.js
+++ tests/Integration/login.test.js
@@ -2,15 +2,16 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const sinon = require('sinon');
 const { MongoClient } = require('mongodb');
-const { getConnection } = require('./connectionMock');
+const { getConnection } = require('../connectionMock');
 
-const server = require('../index');
+const server = require('../../index');
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('POST /createUser', () => {
+
+describe('POST /login', () => {
   before(async () => {
     const connection = await getConnection();
     sinon.stub(MongoClient, 'connect')
@@ -20,36 +21,35 @@ describe('POST /createUser', () => {
   after(async () => {
     await MongoClient.connect.restore();
   });
-  
-  describe('3- Casos de sucesso', () => {
+
+  describe('1- Casos de sucesso', () => {
     let response = {};
     before(async () => {
-      response = await chai.request(server)
+      await chai.request(server)
       .post('/createUser')
+      .send({
+        email: 'test@gmail.com',
+        password: '123456',
+      });
+
+      response = await chai.request(server)
+      .post('/login')
       .send({
         email: 'test@gmail.com',
         password: '123456',
       });
     });
 
-    it('Retorna um objeto', () => {
-      expect(response).to.be.a('object');
+    it('Retorna um token', () => {
+      expect(response.body).to.have.property('token');
     });
 
-    it('Retorna o novo usuário', () => {
-      expect(response.body).to.have.property('id');
-      expect(response.body).to.have.property('email');
-      expect(response.body).to.have.property('password');
-      expect(response.body).to.have.property('role');
-    });
-
-    it('Retorna status 201', () => {
+    it('Retorna status 200', () => {
       expect(response).to.have.property('status');
-      expect(response).to.have.status(201);
+      expect(response).to.have.status(200);
     });
   });
-
-  describe('4- Casos de falha', () => {
+  describe('2- Casos de falha', () => {    
     let response = {};
 
     it('Quando não existir email', async () =>{
@@ -97,32 +97,17 @@ describe('POST /createUser', () => {
       expect(response).to.have.status(400);
       expect(response.body.message).to.equal('"password" length must be at least 6 characters long');
     });
-  });
 
-  describe('5- Quando já existe o email cadastrado', () => {
-    let response = {};
-    before(async () => {
-      await chai.request(server)
-      .post('/createUser')
-      .send({
-        email: 'test@gmail.com',
-        password: '123456',
-      });
-
+    it('Quando usuário não existe', async () =>{
       response = await chai.request(server)
-      .post('/createUser')
+      .post('/login')
       .send({
-        email: 'test@gmail.com',
-        password: '123456',
+        email: 'nao_existe@email.com',
+        password: 'senhaErrada',
       });
-    });
-    it('Retorna status 400', () => {
       expect(response).to.have.property('status');
-      expect(response).to.have.status(400);
-    });
-    it('Retorna mensagem "Email já existe"', () => {
-      expect(response.body).to.have.property('message');
-      expect(response.body.message).to.equal('Email já existe');
+      expect(response).to.have.status(404);
+      expect(response.body.message).to.equal('Incorrect username or password.');
     });
   });
 });
