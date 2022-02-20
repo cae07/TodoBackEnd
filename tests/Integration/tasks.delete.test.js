f2f8b2ef65212a0e11925d9fcff2be910540c31b
Cae <eduardokae01@hotmail.com>
2022-02-20 09:18:06 -0300

    tests: refactor integration tasks.delete

--- tests/Integration/tasks.delete.test.js
+++ tests/Integration/tasks.delete.test.js
@@ -41,7 +41,7 @@ describe('DELETE /tasks', () => {
       });
 
       const addNewTask = await chai.request(server)
-      .post('/tasks')
+      .post('/tasks/newTask')
       .set({ "Authorization": `${body.token}` })
       .send({  task: 'Comprar coquinho'}
       );
@@ -49,7 +49,7 @@ describe('DELETE /tasks', () => {
       taskId = addNewTask.body.id;
       
       response = await chai.request(server)
-      .delete('/tasks')
+      .delete('/tasks/delete')
       .set({ "Authorization": `${body.token}` })
       .send(
         {
@@ -92,7 +92,7 @@ describe('DELETE /tasks', () => {
       token = body.token;
 
       const addNewTask = await chai.request(server)
-      .post('/tasks')
+      .post('/tasks/newTask')
       .set({ "Authorization": token })
       .send({  task: 'Comprar coquinho'}
       );
@@ -103,7 +103,7 @@ describe('DELETE /tasks', () => {
     describe('Quando não existir o campo id', () => {
       before(async () => {
         response = await chai.request(server)
-          .delete('/tasks')
+          .delete('/tasks/delete')
           .set({ "Authorization": token });
       });
 
@@ -121,7 +121,7 @@ describe('DELETE /tasks', () => {
     describe('Caso token não informado', () => {
       before(async () => {
         response = await chai.request(server)
-        .delete('/tasks')
+        .delete('/tasks/delete')
       });
 
       it('Retorna status 401', () => {
