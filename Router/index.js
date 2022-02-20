323f55e98ef423018646149e34bc95d33dbeb6e7
Cae <eduardokae01@hotmail.com>
2022-02-20 18:43:19 -0300

    refactor: userController e tasksController para rotas

--- Router/index.js
+++ Router/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const loginRouter = require('./loginRouter');
-const userRouter = require('./userRouter');
-const tasksRouter = require('./tasksRouter');
 
 const router = express.Router();
 
 router.use('/login', loginRouter);
-router.use('/createUser', userRouter);
-router.use('/tasks', tasksRouter);
+router.use('/createUser', userController);
+router.use('/tasks', tasksController);
 
 module.exports = router;
--- Router/userController.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const userController = require('../Controller/loginController');
-const loginRouter = express.Router();
-
-loginRouter.post('/', userController.POST);
-
-module.exports = loginRouter;
